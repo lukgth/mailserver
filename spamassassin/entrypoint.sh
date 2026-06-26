81193cd294412de0f43baf2315cc3d3d4db3d222
v <v@pibox.local>
2026-06-26 15:56:30 +0000

    fix: spamass-milter uses -D flag (not -s) to connect to remote spamd

--- spamassassin/entrypoint.sh
+++ spamassassin/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 echo "Starting spamass-milter..."
 echo "Connecting to spamd at spamd:783"
 
-# Start spamass-milter connecting to spamd container
-# -p = milter port, -s = spamd socket/host
-spamass-milter -p 9999 -s spamd:783 &
+# -p = milter port
+# -D = connect to remote spamd host
+spamass-milter -p 9999 -D spamd &
 MILTER_PID=$!
 echo "spamass-milter started (PID: $MILTER_PID) -> spamd:783"
 

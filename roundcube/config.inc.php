39bea8eff786a51d76ca7c398dfe8921c915e852
v <v@pibox.local>
2026-06-26 17:14:48 +0000

    fix: roundcube accept self-signed certs on internal network

--- roundcube/config.inc.php
+++ roundcube/config.inc.php
@@ -1,2 +1,18 @@
 <?php
 $config['request_path'] = '/mail/';
+
+// Accept self-signed certs on internal Docker network
+$config['imap_conn_options'] = [
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true,
+    ],
+];
+$config['smtp_conn_options'] = [
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true,
+    ],
+];
